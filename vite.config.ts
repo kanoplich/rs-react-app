79e2ace882fa2c7748f550ca541120f98a2af632
Andrei Kanoplich <andrei.kanoplich@gmail.com>
2026-05-04 19:43:25 +0300

    chore: change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/rs-react-app/',
+  build: {
+    outDir: 'dist',
+  },
   plugins: [react(), tailwindcss(), svgr()],
   resolve: {
     alias: {
