8f46c568397aa0f603040ef525fd9c490d9c3b5a
Andrei Kanoplich <andrei.kanoplich@gmail.com>
2026-05-15 19:33:26 +0300

    refactop: convert header and footer class components to functional

--- src/app/ui/header/index.tsx
+++ src/app/ui/header/index.tsx
@@ -1,11 +1,7 @@
-import { Component } from 'react';
-
-export class Header extends Component {
-  render() {
-    return (
-      <header className=" bg-accent-bg rounded-md flex justify-center">
-        <h1 className="my-2">RS React App</h1>
-      </header>
-    );
-  }
-}
+export const Header = () => {
+  return (
+    <header className=" bg-accent-bg rounded-md flex justify-center">
+      <h1 className="my-2">RS React App</h1>
+    </header>
+  );
+};
