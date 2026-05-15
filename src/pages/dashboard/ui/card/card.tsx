4dd55bc2a703a857994e77621696873b6044a694
Andrei Kanoplich <andrei.kanoplich@gmail.com>
2026-05-15 20:27:50 +0300

    refactor: convert Card, SearchBar and TestErrorButton class components to functional

--- src/pages/dashboard/ui/card/card.tsx
+++ src/pages/dashboard/ui/card/card.tsx
@@ -1,19 +1,13 @@
-import { Component } from 'react';
-
 interface CardProps {
   name: string;
   url: string;
 }
 
-export class Card extends Component<CardProps> {
-  render() {
-    const { name, url } = this.props;
-
-    return (
-      <div className="border border-border rounded-lg p-4 flex flex-col sm:flex-row justify-between my-1">
-        <h3 className="text-lg font-bold text-text-h">{name}</h3>
-        <span className="text-accent hover:underline">{url}</span>
-      </div>
-    );
-  }
-}
+export const Card = ({ name, url }: CardProps) => {
+  return (
+    <div className="border border-border rounded-lg p-4 flex flex-col sm:flex-row justify-between my-1">
+      <h3 className="text-lg font-bold text-text-h">{name}</h3>
+      <span className="text-accent hover:underline">{url}</span>
+    </div>
+  );
+};
