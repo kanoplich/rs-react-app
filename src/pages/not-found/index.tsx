5a2153d21a3ad6c05aceb349063dfd54e72fe48e
Andrei Kanoplich <andrei.kanoplich@gmail.com>
2026-05-17 16:45:34 +0300

    feat: add NotFound and About pages

--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -1,3 +1,17 @@
+import { ROUTES } from '@/shared/config';
+import { Link } from 'react-router-dom';
+
 export const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className="mt-4 flex flex-col items-center">
+      <h2>404 error</h2>
+      <div className="text-xl">page not found</div>
+      <Link
+        to={ROUTES.HOME}
+        className="group inline-flex items-center gap-2 px-3 py-1.5 mt-6 rounded-md hover:bg-accent transition-colors hover:text-text-h"
+      >
+        Go back to Home
+      </Link>
+    </div>
+  );
 };
